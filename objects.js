6d2f86ebdf7035c3c5438a2afddccc143ff1ef72
Eyezallhazy <101095998+Eyezallhazy@users.noreply.github.com>
2022-06-23 23:25:17 +0100

    Initial commit

---
+++ objects.js
@@ -0,0 +1,9 @@
+const info = {
+    name: "Temitope",
+    height: "5:8",
+    country: "Nigeria",
+};
+
+alert(
+    `Hi, my Name is ${info.name}, I'm ${info.height} tall and I'm from ${info.country}`
+);
